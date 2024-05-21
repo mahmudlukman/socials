36dd9384b86fd8107371edcaa4ddc513860ec511
Mahmud Lukman <mistamann49@gmail.com>
2024-05-21 16:12:32 +0100

    database setup complete

---
+++ server/app.ts
@@ -0,0 +1,41 @@
+require('dotenv').config();
+import express, { NextFunction, Request, Response } from 'express';
+export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errorMiddleware } from './middleware/error';
+// import userRouter from './routes/userRouter';
+// import travelRouter from './routes/travelRouter';
+
+// body parser
+app.use(express.json({ limit: '50mb' }));
+app.disable('x-powered-by')
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => Cross Origin Resource Sharing
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
+
+// routes
+// app.use('/api/v1', userRouter);
+// app.use('/api/v1', travelRouter);
+
+// testing API
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ success: true, message: 'API is working' });
+});
+
+// unknown route
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(errorMiddleware);
