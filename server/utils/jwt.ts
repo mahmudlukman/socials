434b56bfcd767fa47ba2684de694fdb1a2249b27
Mahmud Lukman <mistamann49@gmail.com>
2024-05-24 14:44:52 +0100

    jwt added

---
+++ server/utils/jwt.ts
@@ -0,0 +1,42 @@
+require("dotenv").config();
+import { Response } from "express";
+import { IUser } from "../models/User";
+
+interface ITokenOptions {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
+}
+
+// parse environment variables to integrates with fallback values
+const accessTokenExpire = parseInt(
+  process.env.ACCESS_TOKEN_EXPIRE || "300",
+  10
+);
+
+// options for cookies
+ export const accessTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
+  const accessToken = user.SignAccessToken();
+
+  // Only set secure to true in production
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+  }
+
+  res.cookie("access_token", accessToken, accessTokenOptions);
+
+  res.status(statusCode).json({
+    success: true,
+    user,
+    accessToken,
+  });
+};
