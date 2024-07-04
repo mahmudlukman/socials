ebe4b7695cb7f13553838fb884d199c744cc6417
Mahmud Lukman <mistamann49@gmail.com>
2024-07-04 11:35:31 +0100

    user api calls setup complete

--- client/src/redux/features/user/userApi.js
+++ client/src/redux/features/user/userApi.js
@@ -9,9 +9,51 @@ export const userApi = apiSlice.injectEndpoints({
         credentials: 'include',
       }),
     }),
-    deletePost: builder.mutation({
+    editProfile: builder.mutation({
+      query: (data) => ({
+        url: 'update-user',
+        method: 'PUT',
+        body: data,
+        credentials: 'include',
+      }),
+    }),
+    updateUserRole: builder.mutation({
       query: (id) => ({
-        url: `delete/${id}`,
+        url: `update-user-status/${id}`,
+        method: 'PUT',
+        credentials: 'include',
+      }),
+    }),
+    updateProfilePicture: builder.mutation({
+      query: (profilePicture) => ({
+        url: 'update-profile-picture',
+        method: 'PUT',
+        body: {
+          profilePicture,
+        },
+        credentials: 'include',
+      }),
+    }),
+    updateCoverPicture: builder.mutation({
+      query: (coverPicture) => ({
+        url: 'update-cover-picture',
+        method: 'PUT',
+        body: {
+          coverPicture,
+        },
+        credentials: 'include',
+      }),
+    }),
+    getNotifications: builder.query({
+      query: () => ({
+        url: 'get-notifications',
+        method: 'GET',
+        credentials: 'include',
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `delete-user/${id}`,
         method: 'DELETE',
         credentials: 'include',
       }),
